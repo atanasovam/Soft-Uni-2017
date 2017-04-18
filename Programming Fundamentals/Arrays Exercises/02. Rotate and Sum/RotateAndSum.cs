d9f1d7649815089e16d7ccce81b7dff5442b2920
Milena Atanasova <atamasowa40@gmail.com>
2017-04-18 17:29:37 +0300

    Two new ex.

---
+++ Programming Fundamentals/Arrays Exercises/02. Rotate and Sum/RotateAndSum.cs
@@ -0,0 +1,35 @@
+﻿namespace _02.Rotate_and_Sum
+{
+    using System;
+    using System.Linq;
+
+    public class RotateAndSum
+    {
+        public static void Main()
+        {
+            var arr = Console.ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
+            var count = int.Parse(Console.ReadLine());
+
+            var sum = new int[arr.Length];
+
+            for (int r = 0; r < count; r++)
+            {
+                var lastDigit = arr[arr.Length - 1];
+
+                for (int j = arr.Length - 1; j > 0; j--)
+                {
+                    arr[j] = arr[j - 1];
+                }
+
+                arr[0] = lastDigit;
+
+                for (int j = 0; j < sum.Length; j++)
+                {
+                    sum[j] += arr[j];
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", sum));
+        }
+    }
+}
